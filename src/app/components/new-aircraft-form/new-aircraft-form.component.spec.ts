61d92ca63ecff7a50222bb8809b1ddad37a276d7
Anthony Trombetta <atrombet@gmail.com>
2021-01-29 20:08:15 -0600

    Write tests.

--- src/app/components/new-aircraft-form/new-aircraft-form.component.spec.ts
+++ src/app/components/new-aircraft-form/new-aircraft-form.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { NewAircraftFormComponent } from './new-aircraft-form.component';
 
@@ -8,7 +9,8 @@ describe('NewAircraftFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NewAircraftFormComponent ]
+      declarations: [ NewAircraftFormComponent ],
+      imports: [ ReactiveFormsModule ]
     })
     .compileComponents();
   });
--- src/app/services/aircraft.service.spec.ts
+++ src/app/services/aircraft.service.spec.ts
@@ -1,4 +1,8 @@
+// tslint:disable: no-string-literal
 import { TestBed } from '@angular/core/testing';
+import { take } from 'rxjs/operators';
+import { AircraftSize, AircraftType } from '../enums';
+import { Aircraft } from '../interfaces';
 
 import { AircraftService } from './aircraft.service';
 
@@ -18,46 +22,128 @@ describe('AircraftService', () => {
   });
 
   it('should create a new aircraft', () => {
-    expect(true).toBe(false);
+    const { service } = setup();
+    const craft = service.createNewAircraft(AircraftSize.Small, AircraftType.Cargo);
+    expect(craft.type).toBe(AircraftType.Cargo);
+    expect(craft.size).toBe(AircraftSize.Small);
+    expect(craft.hasBeenLaunched).toBe(false);
+    expect(craft.isEnqueued).toBe(false);
   });
 
   it('should enqueue an aircraft', () => {
-    expect(true).toBe(false);
+    const { service } = setup();
+    const craft = service.createNewAircraft(AircraftSize.Small, AircraftType.Cargo);
+    service.enqueue(craft);
+    expect(service['aircraft$'].getValue().get(craft.id)).toBeTruthy();
   });
 
   it('should store all aircraft in state', () => {
-    expect(true).toBe(false);
+    const { service } = setup();
+    service.enqueue(service.createNewAircraft(AircraftSize.Small, AircraftType.Cargo));
+    service.enqueue(service.createNewAircraft(AircraftSize.Small, AircraftType.Passenger));
+    service.enqueue(service.createNewAircraft(AircraftSize.Large, AircraftType.Passenger));
+    expect(service['aircraft$'].getValue().size).toBe(3);
   });
 
-  it('should sort passenger aircraft before cargo aircraft', () => {
-    expect(true).toBe(false);
+  it('should sort passenger aircraft before cargo aircraft', done => {
+    const { service } = setup();
+    service.enqueue(service.createNewAircraft(AircraftSize.Small, AircraftType.Cargo));
+    service.enqueue(service.createNewAircraft(AircraftSize.Small, AircraftType.Passenger));
+
+    service.queue$().pipe(take(1)).subscribe((aircraft: Aircraft[]) => {
+      expect(aircraft[0].type).toBe(AircraftType.Passenger);
+      expect(aircraft[1].type).toBe(AircraftType.Cargo);
+      done();
+    });
   });
 
-  it('should sort large aircraft before small aircraft', () => {
-    expect(true).toBe(false);
+  it('should sort large aircraft before small aircraft', done => {
+    const { service } = setup();
+    service.enqueue(service.createNewAircraft(AircraftSize.Small, AircraftType.Passenger));
+    service.enqueue(service.createNewAircraft(AircraftSize.Large, AircraftType.Passenger));
+
+    service.queue$().pipe(take(1)).subscribe((aircraft: Aircraft[]) => {
+      expect(aircraft[0].size).toBe(AircraftSize.Large);
+      expect(aircraft[1].size).toBe(AircraftSize.Small);
+      done();
+    });
   });
 
   it('should launch an aircraft', () => {
-    expect(true).toBe(false);
+    const { service } = setup();
+    service.enqueue(service.createNewAircraft(AircraftSize.Large, AircraftType.Passenger));
+    service.launch();
+    expect(service['aircraft$'].getValue().get(0).hasBeenLaunched).toBe(true);
+    expect(service['aircraft$'].getValue().get(0).isEnqueued).toBe(false);
   });
 
   it('should dequeue an aircraft', () => {
-    expect(true).toBe(false);
+    const { service } = setup();
+    service.enqueue(service.createNewAircraft(AircraftSize.Large, AircraftType.Passenger));
+    service.dequeue(0);
+    expect(service['aircraft$'].getValue().get(0).hasBeenLaunched).toBe(false);
+    expect(service['aircraft$'].getValue().get(0).isEnqueued).toBe(false);
   });
 
-  it('should return the queue of aircraft', () => {
-    expect(true).toBe(false);
+  it('should return the queue of aircraft', done => {
+    const { service } = setup();
+    service.enqueue(service.createNewAircraft(AircraftSize.Small, AircraftType.Passenger));
+    service.enqueue(service.createNewAircraft(AircraftSize.Large, AircraftType.Cargo));
+    service.enqueue(service.createNewAircraft(AircraftSize.Small, AircraftType.Cargo));
+    service.enqueue(service.createNewAircraft(AircraftSize.Large, AircraftType.Passenger));
+
+    service.queue$().pipe(take(1)).subscribe((aircraft: Aircraft[]) => {
+      expect(aircraft[0].size).toBe(AircraftSize.Large);
+      expect(aircraft[0].type).toBe(AircraftType.Passenger);
+      expect(aircraft[1].size).toBe(AircraftSize.Small);
+      expect(aircraft[1].type).toBe(AircraftType.Passenger);
+      expect(aircraft[2].size).toBe(AircraftSize.Large);
+      expect(aircraft[2].type).toBe(AircraftType.Cargo);
+      expect(aircraft[3].size).toBe(AircraftSize.Small);
+      expect(aircraft[3].type).toBe(AircraftType.Cargo);
+      done();
+    });
   });
 
-  it('should return only launched aircraft', () => {
-    expect(true).toBe(false);
+  it('should return only launched aircraft', done => {
+    const { service } = setup();
+    service.enqueue(service.createNewAircraft(AircraftSize.Small, AircraftType.Passenger));
+    service.enqueue(service.createNewAircraft(AircraftSize.Large, AircraftType.Passenger));
+    service.launch();
+
+    service.launchedAircraft$().pipe(take(1)).subscribe((aircraft: Aircraft[]) => {
+      expect(aircraft.length).toBe(1);
+      expect(aircraft[0].hasBeenLaunched).toBe(true);
+      done();
+    });
   });
 
-  it('should return only dequeued aircraft', () => {
-    expect(true).toBe(false);
+  it('should return only dequeued aircraft', done => {
+    const { service } = setup();
+    service.enqueue(service.createNewAircraft(AircraftSize.Small, AircraftType.Passenger));
+    service.enqueue(service.createNewAircraft(AircraftSize.Large, AircraftType.Passenger));
+    service.dequeue(1);
+
+    service.dequeuedAircraft$().pipe(take(1)).subscribe((aircraft: Aircraft[]) => {
+      expect(aircraft.length).toBe(1);
+      expect(aircraft[0].hasBeenLaunched).toBe(false);
+      expect(aircraft[0].isEnqueued).toBe(false);
+      done();
+    });
   });
 
-  it('should return the next aircraft in the queue', () => {
-    expect(true).toBe(false);
+  it('should return the next aircraft in the queue', done => {
+    const { service } = setup();
+    service.enqueue(service.createNewAircraft(AircraftSize.Small, AircraftType.Passenger));
+    service.enqueue(service.createNewAircraft(AircraftSize.Large, AircraftType.Cargo));
+    service.enqueue(service.createNewAircraft(AircraftSize.Small, AircraftType.Cargo));
+    service.enqueue(service.createNewAircraft(AircraftSize.Large, AircraftType.Passenger));
+
+    service.nextInQueue$().pipe(take(1)).subscribe((aircraft: Aircraft) => {
+      expect(aircraft.size).toBe(AircraftSize.Large);
+      expect(aircraft.type).toBe(AircraftType.Passenger);
+      expect(aircraft.id).toBe(3);
+      done();
+    });
   });
 });
